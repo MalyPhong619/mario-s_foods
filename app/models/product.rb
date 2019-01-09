5e41642a6b2d45fe1c9e75e3a346ea71b6bd5f89
Maly Phongsavanh <phongsavanh619@icloud.com>
2019-01-08 19:26:22 -0800

    renamed :price to :cost, validations past for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
   validates :name, :presence => true
-  validates :price, :presence => nil
+  validates :cost, :presence => true
   validates :origin, :presence => true
 end
--- db/migrate/20190109023320_produce_lists.rb
+++ db/migrate/20190109023320_produce_lists.rb
@@ -2,7 +2,7 @@ class ProduceLists < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
       t.column :name, :string
-      t.column :price, :integer
+      t.column :cost, :integer
       t.column :origin, :string
 
       t.timestamps
