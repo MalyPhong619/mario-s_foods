539b9c64eca13c9cbbe3013e17fbbd4a367a3fd4
Maly Phongsavanh <phongsavanh619@icloud.com>
2019-01-08 18:42:17 -0800

    changes spec and validates presence of name only - pass

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,5 @@
 require 'rails_helper'
 
 describe Product do
-  it { should validate_presence_of :name
-  should validate_presence_of :price
-  should validate_presence_of :origin }
+  it { should validate_presence_of :name }
 end
